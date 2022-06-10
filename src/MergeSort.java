1c8b2467076a744286a3caf08146262141aca567
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-09 22:57:07 -0700

    added a method to print the array

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -60,9 +60,18 @@ public class MergeSort {
             k++;
         }
     }
+    //creating a method to print array
+    public static void print(int[]arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
 
 
     public static void main(String[] args) {
+        int [] arr= {3,25,24,6,2,2,1,5,6,43,6543,9,7};
+
 
     }
 }
