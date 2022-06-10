f7be019d1ec1527924820e03faa35d27d16462b7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-09 21:53:35 -0700

    Initial commit

---
+++ src/MergeSort.java
@@ -0,0 +1,10 @@
+public class MergeSort {
+    public static void mergeSort(int[]arr){
+        int lengthArr = arr.length;
+        int [] leftHalf = 
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
